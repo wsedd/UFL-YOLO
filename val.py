ff3a941042bef8fa0d51d1b1e4013aca510ce212
wsedd <86526913+wsedd@users.noreply.github.com>
2026-08-15 11:15:48 +0800

    initial commit

--- val.py
+++ val.py
@@ -3,7 +3,7 @@ warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO('')  
+    model = YOLO('runs/train/UFL-YOLO/weights/best.pt')  
     model.val(data='dataset/URPC2020/data.yaml',
               split='val',  
               imgsz=640,
@@ -12,5 +12,5 @@ if __name__ == '__main__':
               # rect=False,
               # save_json=True, # if you need to cal coco metrice
               project='runs/val',
-              name='v10n',
+              name='UFL-YOLO',
               )
